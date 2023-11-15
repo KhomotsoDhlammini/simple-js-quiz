25762bc71ba70d41d9ecb72464df17bce0c14967
Jonas Moloto <59583118+Jonasmo12@users.noreply.github.com>
2023-11-15 15:06:25 +0200

    Revert "add user-input page"

--- js/app.js
+++ js/app.js
@@ -108,7 +108,7 @@ var quiz = {
       }
     },
   
-    
+    // (D) OPTION SELECTED
     select: (option) => {
       // (D1) DETACH ALL ONCLICK
       let all = quiz.hAns.getElementsByTagName("label");
